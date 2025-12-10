27c1d4b7ec9fe88c377b648c1d5df7727bb2432b
Kezia Nekoye <keziahnekoye5@gmail.com>
2025-12-10 04:45:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7318090930-b34bf",
+  "appId": "1:860901327706:web:bf8f4ee5a93df79d4ca963",
+  "apiKey": "AIzaSyDCn5P6kBO14SB6VaSK_dETcKIlnof09iI",
+  "authDomain": "studio-7318090930-b34bf.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "860901327706"
+};
