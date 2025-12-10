a4d15bedce3c3ea99a12494d458612c939df175d
Kezia Nekoye <keziahnekoye5@gmail.com>
2025-12-10 08:22:12 +0000

    ---------------------------

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "zinduka2-chatbot",
-  "appId": "1:473529932462:web:758cf2359e833f2f11a469",
-  "apiKey": "AIzaSyC05j5u-LVYj_C_2m1g-f_v4a7o4M6i_t4",
-  "authDomain": "zinduka2-chatbot.firebaseapp.com",
+  "appId": "1:473529932462:web:e0588147d312683911a469",
   "storageBucket": "zinduka2-chatbot.appspot.com",
+  "apiKey": "AIzaSyCCf-c1fC1qBwW01yEtR0c3aO9YyY8hIs4",
+  "authDomain": "zinduka2-chatbot.firebaseapp.com",
   "messagingSenderId": "473529932462"
 };
